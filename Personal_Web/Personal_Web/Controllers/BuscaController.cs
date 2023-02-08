0d0126d4e55644098e8241792cd77602ab8feeb9
gabriel018 <duke.comandos@gmail.com>
2023-02-07 23:29:29 -0300

    O inicio

---
+++ Personal_Web/Personal_Web/Controllers/BuscaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Personal_Web.Models;
+using System.Diagnostics;
+
+namespace Personal_Web.Controllers
+{
+    public class BuscaController : Controller
+    {
+        private readonly ILogger<EstiloController> _logger;
+
+        public BuscaController(ILogger<EstiloController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Personal_Web/Personal_Web/Controllers/ServicosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Personal_Web.Models;
+using System.Diagnostics;
+
+namespace Personal_Web.Controllers
+{
+    public class ServicosController : Controller
+    {
+        private readonly ILogger<ServicosController> _logger;
+
+        public ServicosController(ILogger<ServicosController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}

8ae71298287d283255185ac7cad097a0db0760f2
gabriel018 <duke.comandos@gmail.com>
2023-02-12 21:35:25 -0300

    regex

--- Prover_Web(Basic)/wwwroot/js/site.js
+++ Prover_Web(Basic)/wwwroot/js/site.js
@@ -1,4 +1,40 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+function telefone() {
 
-// Write your JavaScript code.
+    const tel = document.querySelector("#tel")
+ 
+    
+    tel.addEventListener("keypress", () => {
+        const inputlength = tel.value.length
+
+        
+        if (inputlength ===  1) {
+            tel.value.length += '('
+        }
+
+        if (inputlength ===  3 ) {
+            tel.value.length += ')'
+        }
+
+        
+
+       
+    })
+
+
+}
+
+
+
+
+
+
+
+
+//function mascara_telefone(objeto) {
+//    vr = objeto.value;
+//    tam = vr.length;
+//    if (tam < 11) {
+//        objeto.value = '(' + vr.substr(0, 2) + ') ' + vr.substr(2, 4) + '-' + vr.substr(6, 4);
+//    }
+//}

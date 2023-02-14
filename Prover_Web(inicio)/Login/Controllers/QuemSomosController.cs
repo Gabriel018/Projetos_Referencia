12c775a2c37112aea7cb4d022aa3d9729e2b7a88
unknown <duke.comandos@gmail.com>
2023-02-14 17:58:36 -0300

    modal

---
+++ Prover_Web(inicio)/Login/Controllers/QuemSomosController.cs
@@ -0,0 +1,28 @@
+﻿using Login.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Login.Controllers
+{
+    public class QuemSomosController : Controller
+    {
+        private readonly ILogger<QuemSomosController> _logger;
+
+        public QuemSomosController(ILogger<QuemSomosController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+    }
+}

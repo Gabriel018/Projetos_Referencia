955fb1597db07c54b4d557f55c34161a88d49ecd
unknown <duke.comandos@gmail.com>
2023-02-15 17:59:24 -0300

    11

---
+++ Prover_Web(inicio)/Login/Controllers/PlanosController.cs
@@ -0,0 +1,25 @@
+﻿using Login.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Login.Controllers
+{
+
+    [Authorize]
+    public class PlanosController : Controller
+    {
+        private readonly ILogger<PlanosController> _logger;
+
+        public PlanosController(ILogger<PlanosController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Prover_Web(inicio)/Login/Controllers/RelatoriosController.cs
@@ -0,0 +1,32 @@
+﻿using Login.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Login.Controllers
+{
+    public class RelatoriosController : Controller
+    {
+        private readonly ILogger<RelatoriosController> _logger;
+
+        public RelatoriosController(ILogger<RelatoriosController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

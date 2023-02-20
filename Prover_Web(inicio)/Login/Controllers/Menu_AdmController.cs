6d7c5f9bd7f2226aa799c35e0ece64488bb02691
gabriel018 <duke.comandos@gmail.com>
2023-02-20 03:23:52 -0300

    menu

---
+++ Prover_Web(inicio)/Login/Controllers/Menu_AdmController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Login.Controllers
+{
+    public class Menu_AdmController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+}

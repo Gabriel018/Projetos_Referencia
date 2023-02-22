a47e92f980c8674a42ce743e52bfdae26c5062e5
gabriel018 <duke.comandos@gmail.com>
2023-02-21 21:00:14 -0300

    Completo

--- Prover_Web(inicio)/Login/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Prover_Web(inicio)/Login/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,6 +1,4 @@
-﻿
-
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
@@ -73,18 +71,9 @@ namespace Login.Areas.Identity.Pages.Account
         public class InputModel
         {
             /// <summary>
-
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            /// 
-            [Required]
-            [EmailAddress]
-            [Display(Name = "Nome")]
-            public string Nome { get; set; }
-
-
-
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]

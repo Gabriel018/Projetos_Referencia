50175e0c27a9870dd63690dafd872e57e6bb52cf
gabriel018 <duke.comandos@gmail.com>
2023-02-22 23:13:51 -0300

    modal

--- Prover_Web(inicio)/Login/wwwroot/js/site.js
+++ Prover_Web(inicio)/Login/wwwroot/js/site.js
@@ -5,3 +5,31 @@
 
 /*window.onload = function () { alert("Está carregado!") } */
 
+const notific = document.querySelector("#notific")
+
+var modal = document.getElementById("myModal");
+
+var span = document.getElementsByClassName("close")[0];
+
+function modall() {
+    notific.addEventListener("click", function () {
+        modal.style.display = "block";
+    })
+
+
+}
+
+
+//notific.onclick = function () {
+   
+//}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
